4a8d5554e9ae6c06dc5c16a29ec402a6bb79722f
Ruddytip <anton.rudakov216@mail.ru>
2021-09-01 15:44:49 +0300

    add file

--- Makefile
+++ Makefile
@@ -1,10 +1,23 @@
-SRC := $(wildcard *.cpp)
-CFLAGS = -I. -Wall -std=c++2a
+SYSCONF_LINK = g++
+CPPFLAGS     =
+LDFLAGS      =
+LIBS         = -lm
 
-all: test
+DESTDIR = ./
+TARGET  = main
 
-test: $(SRC)
-	g++ -o $@ $^ $(CFLAGS)
+OBJECTS := $(patsubst %.cpp,%.o,$(wildcard *.cpp))
+
+all: $(DESTDIR)$(TARGET)
+
+$(DESTDIR)$(TARGET): $(OBJECTS)
+	$(SYSCONF_LINK) -Wall $(LDFLAGS) -o $(DESTDIR)$(TARGET) $(OBJECTS) $(LIBS)
+
+$(OBJECTS): %.o: %.cpp
+	$(SYSCONF_LINK) -Wall $(CPPFLAGS) -c $(CFLAGS) $< -o $@
 
 clean:
-	rm test
+	-rm -f $(OBJECTS)
+	-rm -f $(TARGET)
+	-rm -f *.tga
+
---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "Graphics.hpp"
+
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red   = TGAColor(255, 0,   0,   255);
+Model *model = NULL;
+const int width  = 800;
+const int height = 800;
+const char* name("test.tga");
+
+int main(int argc, char** argv) {
+    if (2==argc) {
+        model = new Model(argv[1]);
+    } else {
+        model = new Model("obj/african_head.obj");
+    }
+
+	Scene image(width, height, TGAImage::RGB, name);
+	
+    for (int i=0; i<model->nfaces(); i++) {
+        std::vector<int> face = model->face(i);
+        for (int j=0; j<3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j+1)%3]);
+            int x0 = (v0.x+1.)*width/2.;
+            int y0 = (v0.y+1.)*height/2.;
+            int x1 = (v1.x+1.)*width/2.;
+            int y1 = (v1.y+1.)*height/2.;
+            image.line(x0, y0, x1, y1, white);
+        }
+    }
+	
+	image.save();
+    delete model;
+    return 0;
+}
+
